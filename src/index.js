28dd92b991f2c6420f4ba6220343e125ad273cf5
k.abramkin <k.abramkin@kernel.ua>
2023-12-28 20:20:52 +0200

    crate table 10*10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 document.addEventListener("DOMContentLoaded", documentOnReady);
 
 function documentOnReady() {
-  const checkThemeEl = document.getElementById("checkTheme");
+  // get html elements
+  const checkThemeEl = getEl("#checkTheme");
+  const smileWrapEl = getEl("#smileWrapEl");
+  const tabelEl = getEl("#table");
+  console.log("get el", tabelEl);
+  //vars
+
+  function createTable(count = 10) {
+    const tableCount = count;
+    for (let i = 0; i < tableCount; i++) {
+      const rowEl = createEl("tr");
+      for (let j = 0; j < tableCount; j++) {
+        const tdEl = createEl("td");
+        tdEl.innerHTML = j + 1 + i * 10;
+        rowEl.appendChild(tdEl);
+      }
+      tabelEl.appendChild(rowEl);
+    }
+  }
+
+  createTable(10);
+
   const black = "#151515";
   const white = "#aaaaaa";
   const arrSmiles = [
@@ -18,7 +39,7 @@ function documentOnReady() {
     "🇻🇬",
     "🍯",
   ];
-  const smileWrapEl = document.getElementById("smileWrapEl");
+  //
   checkThemeEl.addEventListener("change", function () {
     const styleObj = document.documentElement.style;
     if (checkThemeEl.checked) {
@@ -36,4 +57,19 @@ function documentOnReady() {
     newEl.classList.add("smile-card");
     smileWrapEl.appendChild(newEl);
   });
+}
+
+function getEl(id) {
+  const symbol = id[0];
+  if (symbol === "#") {
+    return document.getElementById(id.slice(1));
+  } else if (symbol === ".") {
+    return document.getElementsByClassName(id.slice(1));
+  } else {
+    return document.getElementsByTagName(id);
+  }
+}
+
+function createEl(tag) {
+  return document.createElement(tag);
 }
